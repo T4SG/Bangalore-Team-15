a247fafe5e64bfbe11b0a3c06fea034ef712d869
Shravani Sagar Thadisetty <sagar.shravani10@gmail.com>
2015-07-25 20:16:08 +0530

    Admin login page

---
+++ adminlogin.php
@@ -0,0 +1,27 @@
+
+
+<?php
+$con=mysql_connect("127.0.0.1","root","code4good")
+or die('Error connecting to mysql: '.mysql_error());
+
+
+mysql_select_db("code4good",$con);
+
+$id=$_POST['uid'];
+$pass=$_POST['pass'];
+
+
+$r=mysql_query("select pass from admin where name='$id'");
+if($r==null)
+{
+ echo 'wrong username or password';
+}
+else
+{
+ header("Location: ec2-54-169-129-45.ap-southeast-1.compute.amazonaws.com/admin.html");
+}
+
+
+mysql_close($con);
+?>
+
