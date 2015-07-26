9be0345568faf67be772918319c7c6aa6bfc46c0
Shravani Sagar Thadisetty <sagar.shravani10@gmail.com>
2015-07-26 05:44:21 +0530

    Filter file added

---
+++ filter.php
@@ -0,0 +1,44 @@
+<?php
+$con=mysql_connect("127.0.0.1","root","code4good")
+or die('Error connecting to mysql: '.mysql_error());
+mysql_select_db("code4good",$con);
+
+//$num=$_POST('number');
+
+
+//$id=$_POST['cat'];
+//$response=array();
+//$post=array();
+
+$new=[];
+$d=mysql_query("select name from mentee where status='pending'");
+echo 'hey';
+while($bigrow=mysql_fetch_assoc($d))
+{
+ $name=$bigrow['name'];
+echo $name;
+ 
+$r=mysql_query("select e.email,e.name,e.phone from mentor as e,mentee as p,menski as m,menlan as l where e.email=m.email and e.email=l.email and p.name='$name' and p.language=l.language and p.interest=m.skill");
+
+while($row=mysql_fetch_assoc($r))
+		
+{
+$out[]=$row;
+echo 'hello';	
+        
+	$user_name=$row['name'];
+	$user_email=$row['email'];
+	$user_phone=$row['phone'];
+	
+	
+	$post[]=array('username'=>$user_name,'useremail'=>$user_email,'userphone'=>$user_phone);
+        $new[$name=>$post[]];
+
+}
+//$p=mysql_fetch_array($post);
+//$response['feed']=$post;
+}
+echo json_encode($new);
+mysql_close($con);
+?>
+
